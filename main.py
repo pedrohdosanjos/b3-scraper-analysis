b56a86865b38967c0bfa8ea91b49fe0a0621a975
Pedro Henrique Góes dos Anjos <phganjos@mppr.mp.br>
2025-05-27 14:01:04 -0300

    main roda a interface

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from analysis import run_analysis
+from interface import run_interface
 from scraper import run_scraper
 from utils.logging_service import setup_logger
 
 logger = setup_logger("scraper", "scraper.log")
 
+
+def main():
+    run_interface(logger)
+
+
 if __name__ == "__main__":
-    run_scraper(logger)
+    main()
